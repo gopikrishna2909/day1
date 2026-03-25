f51d876e99bbdac446434ff9c7f75cc6d1ead43e
Tumma <gtumma@deloitte.com>
2026-03-25 12:29:13 +0530

    completed Caesar Cipher Code

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,23 +1,33 @@
-from operator import index
+from art import  logo
 
 alphabets=['a','b', 'c', 'd' ,'e', 'f', 'g' ,'h', 'i','j', 'k', 'l' ,'m' ,'n', 'o', 'p' ,'q' ,'r',
          's' ,'t' ,'u' ,'v','w', 'x', 'y', 'z']
-direction = input("Type 'encrypt' to Encrypt and 'decrypt' to Decrypt:\n" )
-text = input("Enter the text:\n").lower()
-shift = int(input("Enter the shift number:\n"))
+print(logo)
 
-def encrypt(original_text, shift_amount):
-    #alphabests_index = 0
-    encrypted_text = ""
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
     for i in original_text:
-        alphabests_index = alphabets.index(i)+shift
-        # if alphabests_index <= 26:
-        #     encrypted_text += alphabets[alphabests_index]
-        # else:
-        #     encrypted_text += alphabets[(alphabets.index(i)+shift)-26]
-        alphabests_index %= len(alphabets)
-        encrypted_text += alphabets[alphabests_index]
+        if i not in alphabets:
+            output_text += i
+        else:
+            shifted_index = alphabets.index(i) + shift_amount
+            shifted_index %= len(alphabets)
+            output_text += alphabets[shifted_index]
+
+    print(f"your {encode_or_decode}d text is: {output_text}")
 
 
-    print(encrypted_text)
-encrypt(original_text=text,shift_amount=shift)
+
+run = True
+while run:
+    direction = input("Type encrypt to Encrypt and decrypt to Decrypt:\n" ).lower()
+    text = input("Enter the text:\n").lower()
+    shift = int(input("Enter the shift number:\n"))
+    caesar(text, shift, direction)
+    user_choice = input("Type yes to continue otherwise no to stop\n").lower()
+    if user_choice == "no":
+        run = False
+        print("Good Bye!")
